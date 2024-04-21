dd0f79ee64965225b3bca9ce71270501d0351086
shieldadchr <shielda.diasavitri@gmail.com>
2024-04-21 19:36:58 +0700

    Mengubah validasi email menjadi lebih langsung dan lebih sederhana

--- Tubes_kelompok6-main/AplikasiHotel/Dashboard.cs
+++ Tubes_kelompok6-main/AplikasiHotel/Dashboard.cs
@@ -494,19 +494,8 @@ namespace AplikasiHotel
             }
         }
         // Method untuk memvalidasi jika email yang di masukan valid atau tidak
-        private bool IsValidEmail(string email)
-        {
-            try
-            {
-                // Mengecek jika inputan user sesuai format email yang benar
-                var mailAddress = new System.Net.Mail.MailAddress(email);
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
+        private bool IsValidEmail(string email) => new System.Net.Mail.MailAddress(email).Address == email;
+
 
         private void SavePesananData()
         {
